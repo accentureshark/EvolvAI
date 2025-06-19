c8371fb30acaea87edff4ddc9f4fab7063a2c73c
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-06-19 12:18:12 -0300

    refactor(admin\model): refactor code for pass Checkstyle and PMD

--- rag/src/main/java/org/shark/evolvai/admin/model/EmbeddingSource.java
+++ rag/src/main/java/org/shark/evolvai/admin/model/EmbeddingSource.java
@@ -1,12 +1,16 @@
 package org.shark.evolvai.admin.model;
 
-import jakarta.persistence.*;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.UUID;
 
 @Entity
 @Table(name = "embedding_source", schema = "admin")

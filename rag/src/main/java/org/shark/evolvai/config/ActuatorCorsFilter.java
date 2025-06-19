5eb726f750f1056dc9a22a94e320c363d2baf8d5
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-06-19 12:22:49 -0300

    refactor(config): refactor code for pass Checkstyle and PMD reports

--- rag/src/main/java/org/shark/evolvai/config/ActuatorCorsFilter.java
+++ rag/src/main/java/org/shark/evolvai/config/ActuatorCorsFilter.java
@@ -1,12 +1,16 @@
 package org.shark.evolvai.config;
 
 
-import jakarta.servlet.*;
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Component
 public class ActuatorCorsFilter implements Filter {

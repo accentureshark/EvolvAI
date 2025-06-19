bb0c085d39b50eaa9c65927d50e637ab6898831c
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-06-19 12:36:56 -0300

    refactor(llm): refactor code for success Checkstyle and PMD reports

--- rag/src/main/java/org/shark/evolvai/llm/exception/ModelNotLoadedException.java
+++ rag/src/main/java/org/shark/evolvai/llm/exception/ModelNotLoadedException.java
@@ -2,6 +2,10 @@ package org.shark.evolvai.llm.exception;
 
 public class ModelNotLoadedException extends RuntimeException {
     public ModelNotLoadedException(String modelName) {
-        super("El modelo Ollama '" + modelName + "' no está cargado. Por favor, cárgalo antes de consultar.");
+        super(
+            "El modelo Ollama '"
+                + modelName
+                + "' no está cargado. Por favor, cárgalo antes de consultar."
+        );
     }
 }
--- rag/src/main/java/org/shark/evolvai/llm/port/out/LlmProvider.java
+++ rag/src/main/java/org/shark/evolvai/llm/port/out/LlmProvider.java
@@ -1,14 +1,14 @@
 package org.shark.evolvai.llm.port.out;
 
-import dev.langchain4j.data.message.ChatMessage;
-
 import java.util.List;
+
+import dev.langchain4j.data.message.ChatMessage;
 
 public interface LlmProvider {
 
     ChatMessage send(List<ChatMessage> messages);
 
-    public String generateResponse(List<ChatMessage> context, String query) ;
+    String generateResponse(List<ChatMessage> context, String query);
 
     String getDefaultPrompt();
 }

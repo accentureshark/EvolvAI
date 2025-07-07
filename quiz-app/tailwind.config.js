e08165fb94bb005a49658275aa07add5b109de2c
pablooromero <pabloromerook@gmail.com>
2025-07-07 13:47:22 -0300

    refactor: removed typescript

--- quiz-app/tailwind.config.js
+++ quiz-app/tailwind.config.js
@@ -1,13 +1,10 @@
-
-import type { Config } from "tailwindcss";
-
 export default {
 	darkMode: ["class"],
 	content: [
-		"./pages/**/*.{ts,tsx}",
-		"./components/**/*.{ts,tsx}",
-		"./app/**/*.{ts,tsx}",
-		"./src/**/*.{ts,tsx}",
+		"./pages/**/*.{js,jsx}",
+		"./components/**/*.{js,jsx}",
+		"./app/**/*.{js,jsx}",
+		"./src/**/*.{js,jsx}",
 	],
 	prefix: "",
 	theme: {
@@ -62,4 +59,4 @@ export default {
 		}
 	},
 	plugins: [],
-} satisfies Config;
+};

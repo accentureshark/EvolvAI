33e7a2ed0561df5d94566116b372cc980b33da02
faguero <faguero@gmail.com>
2025-07-11 19:37:06 -0300

    feat: add packaging type to pom.xml for JAR deployment

--- quiz/src/main/java/org/shark/quizai/domain/port/out/QuizRepository.java
+++
@@ -1,7 +0,0 @@
-package org.shark.quizai.domain.port.out;
-
-import org.shark.quizai.domain.model.QuizDefinition;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizRepository extends JpaRepository<QuizDefinition, String> {
-}
